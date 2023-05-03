3761533e3035913ba70e53056b8fdb11e7a03246
이정욱 <65989344+ukly@users.noreply.github.com>
2023-05-03 21:19:14 +0900

    1181번 단어 정렬 문제

--- wink-study/2주차/1181.py
+++ wink-study/2주차/1181.py
@@ -6,7 +6,7 @@ for i in range(N):
     w = input()
     if w not in str_list:
         str_list.append(w)
-str_list.sort()
-str_list.sort(key=len)
+str_list.sort()     #우선 사전순으로 정렬
+str_list.sort(key=len)  #그 뒤에 길이순으로 정렬 해준다.
 for n in str_list:
     print(n, end='')
