8eaddfe77d0c85e712c4b44eb22372e4108f5293
이정욱 <65989344+ukly@users.noreply.github.com>
2023-05-18 11:42:50 +0900

    11399번 ATM 문제

--- wink-study/4주차/11399.py
+++ wink-study/4주차/11399.py
@@ -5,9 +5,9 @@ n = int(input())
 times = list(map(int, input().split()))
 res = 0
 
-times.sort()           //걸리는 시간의 최소 순으로 정렬
+times.sort()           #걸리는 시간의 최소 순으로 정렬
 
 for i in range(n):
-    res += sum(times[:i+1])  //계산할 사람 + 그 앞순서의 사람들 값의 합
+    res += sum(times[:i+1])  #계산할 사람 + 그 앞순서의 사람들 값의 합
 
 print(res)
