443907cd39c5e9914a141f3be3b9afaa608d3e44
이정욱 <65989344+ukly@users.noreply.github.com>
2023-06-22 19:12:33 +0900

    Update 1149.py

--- wink-study/6주차/1149.py
+++ wink-study/6주차/1149.py
@@ -13,4 +13,4 @@ for i in range(1, n+1):
     costB = min(rgbOfHouses[i-1][0], rgbOfHouses[i-1][1]) + b   # i번째에 b를 선택했을 때의 cost
     rgbOfHouses.append([costR, costG, costB])                   # i번째에 각 선택에 따른 cost를 저장
 
-print(min(min(rgbOfHouses[n][0], rgbOfHouses[n][1]), rgbOfHouses[n][2])) 가장 cost 누적이 작은 값을 선택
+print(min(min(rgbOfHouses[n][0], rgbOfHouses[n][1]), rgbOfHouses[n][2])) #가장 cost 누적이 작은 값을 선택
